8bb8fbce8d554cd10d7362770c747837bdc4b01e
mc <mcapsan@gmail.com>
2024-05-25 07:22:25 -0400

    Modify gridCell size to fit in gridContainer

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+const gridSize = 9;
+
 function createGrid(size) {
 	let grid = [];
 	for (let i = 0; i < size; i++) {
 		grid.push([]);
 		for (let j = 0; j < size; j++) {
-			grid[i].push(document.createElement("div"));
+			gridCell = document.createElement("div");
+			gridCell.style.border = "solid black";
+			gridCell.style.width = `${100/size}%`;
+			gridCell.style.height = `${100/size}%`;
+			gridCell.addEventListener("mouseover", (e) => {
+				e.target.style.backgroundColor = "black";	
+			});
+			grid[i].push(gridCell);
 		}
 	}
 	return grid;
 }
 
+gridContainer = document.querySelector(".grid-container");
+grid = createGrid(gridSize);
+
+grid.flat().forEach((gridCell) => {
+	gridContainer.appendChild(gridCell);
+});
